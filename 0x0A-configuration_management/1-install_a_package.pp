fd4cb960b355df6a70064a8d5459e5401e057b83
Trevor800 <jeynator780@gmail.com>
2023-08-26 02:56:07 -0700

    my commits

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Task 1 - Install flask v. 2.1.0
-package { 'python flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3'
 }
